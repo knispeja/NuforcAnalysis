ec4c351c17f8224f2d00b3ac19fd186996f9a8e6
Jacob Knispel <knispel.jacob@gmail.com>
2020-05-05 23:29:24 -0500

    Improved error handling

--- scrape_nuforc.py
+++ scrape_nuforc.py
@@ -62,58 +62,76 @@ with open(destination, "w", newline='', encoding='utf-8') as output_file:
 
         # Read through the table of reports by date and get a report
         for report_link_soup in reports_by_date_soup.find('table').find_all('a'):
-            link_to_report_page = report_link_soup.attrs['href']
-            report_reader = url_open_with_retry(root_url + link_to_report_page)
-            if (report_reader == None):
-                print(" -- Failed: " + link_to_report_page)
-                continue
+            try:
+                link_to_report_page = report_link_soup.attrs['href']
+                report_reader = url_open_with_retry(root_url + link_to_report_page)
+                if (report_reader == None):
+                    print(" -- " + link_to_report_page)
+                    continue
 
-            report_html = report_reader.read()
-            report_soup = BeautifulSoup(report_html, 'html.parser').find('tbody')
+                report_html = report_reader.read()
+                report_soup = BeautifulSoup(report_html, 'html.parser').find('tbody')
 
-            # Parse report metadata
-            report_metadata_soup = report_soup.find('td')
-            report_metadata_text = report_metadata_soup.get_text(separator='|')
-            
-            mod_text = report_metadata_text[report_metadata_text.find(':') + 1:]
-            date_occurred = mod_text[:mod_text.find('|')].strip()
+                if (report_soup == None):
+                    print(" -- Failed at empty page: " + link_to_report_page)
+                    continue
 
-            mod_text = mod_text[mod_text.find('|') + 1:]
-            mod_text = mod_text[mod_text.find(':') + 1:]
-            date_reported = mod_text[:mod_text.find('|')].strip()
+                # Parse report metadata
+                report_metadata_soup = report_soup.find('td')
+                if (report_metadata_soup == None):
+                    print(" -- Failed at page missing metadata: " + link_to_report_page)
+                    continue
 
-            mod_text = mod_text[mod_text.find('|') + 1:]
-            mod_text = mod_text[mod_text.find(':') + 1:]
-            date_posted = mod_text[:mod_text.find('|')].strip()
+                report_metadata_text = report_metadata_soup.get_text(separator='|')
+                
+                mod_text = report_metadata_text[report_metadata_text.find(':') + 1:]
+                date_occurred = mod_text[:mod_text.find('|')].strip()
 
-            mod_text = mod_text[mod_text.find('|') + 1:]
-            mod_text = mod_text[mod_text.find(':') + 1:]
-            location = mod_text[:mod_text.find('|')].strip()
+                mod_text = mod_text[mod_text.find('|') + 1:]
+                mod_text = mod_text[mod_text.find(':') + 1:]
+                date_reported = mod_text[:mod_text.find('|')].strip()
 
-            mod_text = mod_text[mod_text.find('|') + 1:]
-            mod_text = mod_text[mod_text.find(':') + 1:]
-            shape = mod_text[:mod_text.find('|')].strip()
+                mod_text = mod_text[mod_text.find('|') + 1:]
+                mod_text = mod_text[mod_text.find(':') + 1:]
+                date_posted = mod_text[:mod_text.find('|')].strip()
 
-            mod_text = mod_text[mod_text.find('|') + 1:]
-            mod_text = mod_text[mod_text.find(':') + 1:]
-            duration = mod_text.strip()
+                mod_text = mod_text[mod_text.find('|') + 1:]
+                mod_text = mod_text[mod_text.find(':') + 1:]
+                location = mod_text[:mod_text.find('|')].strip()
 
-            # Parse report text
-            report_text_soup = report_metadata_soup.find_next('td')
-            report_text = report_text_soup.get_text(separator='\n')
-            report_text = report_text.replace('|', '/')
-            report_text = report_text.replace('\"', '\'')
-            report_text = report_text.strip()
+                mod_text = mod_text[mod_text.find('|') + 1:]
+                mod_text = mod_text[mod_text.find(':') + 1:]
+                shape = mod_text[:mod_text.find('|')].strip()
 
-            # Write to CSV
-            output_writer.writerow({
-                'event_date': date_occurred,
-                'reported_date': date_reported,
-                'posted_date': date_posted,
-                'location': location,
-                'shape': shape,
-                'duration': duration,
-                'text': report_text
-            })
+                mod_text = mod_text[mod_text.find('|') + 1:]
+                mod_text = mod_text[mod_text.find(':') + 1:]
+                duration = mod_text.strip()
 
-            print('.', end='', flush=True)
+                # Parse report text
+                report_text_soup = report_metadata_soup.find_next('td')
+                if (report_text_soup == None):
+                    print(" -- Failed at page missing metadata: " + link_to_report_page)
+                    continue
+
+                report_text = report_text_soup.get_text(separator='\n')
+                report_text = report_text.replace('|', '/')
+                report_text = report_text.replace('\"', '\'')
+                report_text = report_text.strip()
+
+                # Write to CSV
+                output_writer.writerow({
+                    'event_date': date_occurred,
+                    'reported_date': date_reported,
+                    'posted_date': date_posted,
+                    'location': location,
+                    'shape': shape,
+                    'duration': duration,
+                    'text': report_text
+                })
+
+                # Runs slower with this included
+                # print('.', end='', flush=True)
+
+            # Be lenient so that aberrant pages don't kill the script
+            except Exception as err:
+                print("-- Fatal error with this report")
